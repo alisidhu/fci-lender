48c88556d761774aab9d9ae4aeb57aa6840fe1bb
Ali <aliisidhu@gmail.com>
2024-10-06 02:49:00 +0500

    main login

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/LoginScreen` | `/(tabs)/explore` | `/ForgotPasswordScreen` | `/ForgotUserNameScreen` | `/LoginScreen` | `/MySplashScreen` | `/TutorialScreen` | `/_sitemap` | `/explore`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/explore` | `/ForgotPasswordScreen` | `/ForgotUserNameScreen` | `/LoginScreen` | `/MySplashScreen` | `/TutorialScreen` | `/_sitemap` | `/explore`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
